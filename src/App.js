645999563376dd8e1e8fc78ca1356ffc84a7ac03
Jhonatan Alarcón <jhonatan.alarcon.ariza@gmail.com>
2021-05-18 14:09:57 -0500

    Init commit - example d3 pie

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Pie from './components/pie-chart'
+
+const data = [
+  {item: 'Redmi', count: 490},
+  {item: 'Huawei', count: 291},
+  {item: 'IPhone', count: 348},
+  {item: 'Samsung', count: 245},
+  {item: 'Pixel', count: 56}
+];
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>D3 demo</h1>
+    <Pie data={data}/>
+    </>
   );
 }
 
