6c1f17033547726a68ece5e5f8c75b90f8cb3b84
Jhonatan Alarcón <jhonatan.alarcon.ariza@gmail.com>
2022-05-23 17:56:41 -0500

    Improvements styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import './App.css';
-import Pie from './components/pie-chart'
+import Pie from './components/pie-chart';
 
 const data = [
-  {item: 'Redmi', count: 490},
-  {item: 'Huawei', count: 291},
-  {item: 'IPhone', count: 348},
-  {item: 'Samsung', count: 245},
-  {item: 'Pixel', count: 56}
+  { item: 'Redmi', count: 490 },
+  { item: 'Huawei', count: 291 },
+  { item: 'IPhone', count: 348 },
+  { item: 'Samsung', count: 245 },
+  { item: 'Pixel', count: 50 },
 ];
-
-
 
 function App() {
   return (
     <>
-    <h1>D3 demo</h1>
-    <Pie data={data}/>
+      <h1>D3 demo</h1>
+      <Pie data={data} />
     </>
   );
 }
--- src/components/pie-chart.js
+++ src/components/pie-chart.js
@@ -1,76 +1,82 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
+const Pie = ({ data, width = 600, height = 600 }) => {
+  const pieChart = useRef();
 
+  useEffect(() => {
+    // Get positions for each data object
+    const piedata = d3.pie().value((d) => d.count)(data);
+    console.log('piedata', piedata);
+    // Define arcs for graphing
+    const arc = d3.arc().innerRadius(0).outerRadius(200);
+    // Define scale ordinal - discrete
+    const colors = d3.scaleOrdinal([
+      '#ffa822',
+      '#134e6f',
+      '#ff6150',
+      '#1ac0c6',
+      '#dee0e6',
+    ]);
 
-const Pie = ({data, width=600, height=600}) => {
+    // Define the size and position of svg
+    const svg = d3
+      .select(pieChart.current)
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', 'translate(300,300)');
 
-	const pieChart = useRef()
+    // Tooltip
+    const tooldiv = d3
+      .select('#chartArea')
+      .append('div')
+      .style('visibility', 'hidden')
+      .style('position', 'absolute')
+      .style('background-color', 'gray');
 
-	useEffect(()=>{
+    // Draw pie
+    svg
+      .append('g')
+      .selectAll('path')
+      .data(piedata)
+      .join('path')
+      .attr('d', arc)
+      .attr('fill', (d, i) => colors(i))
+      .attr('stroke', 'white')
+      .on('mouseover', (e, d) => {
+        console.log(e);
+        console.log(d);
 
-		// Get positions for each data object
-		const piedata = d3.pie().value(d => d.count)(data)
-        console.log('piedata',piedata)
-		// Define arcs for graphing 
-		const arc = d3.arc().innerRadius(0).outerRadius(200)
-        // Define scale ordinal - discrete
-		const colors = d3.scaleOrdinal(['#ffa822','#134e6f','#ff6150','#1ac0c6','#dee0e6'])
-       
-		// Define the size and position of svg
-		const svg = d3.select(pieChart.current)
-						.attr('width', width)
-						.attr('height', height)
-						.append('g')
-							.attr('transform','translate(300,300)')
+        tooldiv
+          .style('visibility', 'visible')
+          .text(`${d.data.item}:` + `${d.data.count}`);
+      })
+      .on('mousemove', (e, d) => {
+        tooldiv
+          .style('top', e.pageY - 50 + 'px')
+          .style('left', e.pageX - 50 + 'px');
+      })
+      .on('mouseout', () => {
+        tooldiv.style('visibility', 'hidden');
+      });
 
-		// Tooltip
-		const tooldiv = d3.select('#chartArea')
-						  .append('div')
-						  .style('visibility','hidden')
-						  .style('position','absolute')
-						  .style('background-color','red')
+    // Add title
+    svg
+      .append('text')
+      .attr('x', '0px')
+      .attr('y', '-250px')
+      .attr('text-anchor', 'middle')
+      .attr('font-size', '16px')
+      .attr('fill', 'black')
+      .text('Pie example');
+  });
 
-
-		// Draw pie
-		svg.append('g')
-			.selectAll('path')
-			.data(piedata)
-			.join('path')
-				.attr('d', arc)
-				.attr('fill',(d,i)=>colors(i))
-				.attr('stroke', 'white')
-				.on('mouseover', (e,d)=>{
-					console.log(e)
-					console.log(d)
-
-					tooldiv.style('visibility','visible')
-							.text(`${d.data.item}:` + `${d.data.count}`)
-				})
-				.on('mousemove', (e,d)=>{
-					tooldiv.style('top', (e.pageY-50) + 'px')
-							.style('left', (e.pageX-50) + 'px')
-				})
-				.on('mouseout',()=>{
-					tooldiv.style('visibility','hidden')
-				})
-
-        // Add title 
-        svg.append('text')
-        .attr('x','0px')
-        .attr('y', '-250px')
-        .attr('text-anchor', 'middle')
-        .attr('font-size', '16px')
-        .attr('fill','black')
-        .text('Pie example')
-
-	})
-
-	return (
-		<div id='chartArea'>
-			<svg ref={pieChart}></svg>
-		</div>
-	)
-}
+  return (
+    <div id='chartArea'>
+      <svg ref={pieChart}></svg>
+    </div>
+  );
+};
 
 export default Pie;
